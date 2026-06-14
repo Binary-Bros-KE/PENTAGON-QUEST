adb4e4586758bc603bbd22ecacd709309ea0e2af
Nyongesa Evans <nyongesaevans881@gmai.com>
2026-06-14 21:23:36 +0300

    picture remove

--- src/pages/Home/galleryData.js
+++ src/pages/Home/galleryData.js
@@ -1,6 +1,5 @@
 export const galleryImages = [
-  ...Array.from({ length: 19 }, (_, index) => `/gallery/img-${index + 1}.jpeg`),
-  ...[21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42].map(
+  ...[1, 6, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42].map(
     (imageNumber) => `/gallery/img-${imageNumber}.jpeg`
   ),
   '/gallery/img-43.png',
@@ -23,11 +22,9 @@ export const watermarkedGalleryImages = [
   '/gallery/img-17.jpeg',
   '/gallery/img-18.jpeg',
   '/gallery/img-19.jpeg',
-  '/gallery/img-20.jpeg',
   '/gallery/img-21.jpeg',
   '/gallery/img-22.jpeg',
   '/gallery/img-23.jpeg',
-  '/gallery/img-24.jpeg',
   '/gallery/img-25.jpeg',
   '/gallery/img-26.jpeg',
   '/gallery/img-27.jpeg',
